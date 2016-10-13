5a5e86c22036e94d956dd21e94ea88c1222865e2
Artem Boyur <gboyur@gmail.com>
2016-10-13 11:59:06 +0300

    portfolio -> node

--- server.js
+++ server.js
@@ -1,8 +1,62 @@
-var http = require('http');
-
+var http = require('http'),
+  fs = require('fs'),
+  url = require('url'),
+  path = require('path'),
+  typeMime = {
+    '.html': 'text/html',
+    '.htm': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.ttf': 'application/ttf',
+    '.eot': 'application/font-eot',
+    '.otf': 'font/opentype',
+    '.woff': 'application/x-font-wof',
+    '.woff2': 'application/font-woff2'
+  };
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080);
+  var _url = url.parse(req.url),
+    filename = 'public/' + _url.pathname.substring(1),
+    extname,
+    type,
+    img,
+    font;
+  if (_url.pathname === '/') {
+    filename = 'public/index.html';
+  }
+  extname = path.extname(filename);
+  type = typeMime[path.extname(filename)];
 
-console.log('Server running on port 8080.');
+  if ((extname === '.png') || (extname === '.jpg')) {
+    img = fs.readFileSync(filename);
+    res.writeHead(200, {
+      'Content-Type': type
+    });
+    res.write(img, 'hex');
+    res.end();
+  } else if ((extname === '.woff2') || (extname === '.woff') || (extname === '.ttf')) {
+    font = fs.readFileSync(filename);
+    res.writeHead(200, {
+      'Content-Type': type
+    });
+    res.write(font, 'binary');
+    res.end();
+  } else {
+    fs.readFile(filename, 'utf-8', function (err, content) {
+      if (err) {
+        res.writeHead(404, {
+          'Content-Type': 'text/plain; charset=utf-8'
+        });
+        res.write(err.message);
+        res.end();
+      } else {
+        res.writeHead(200, {
+          'Content-Type': type
+        });
+        res.write(content);
+        res.end();
+      }
+    })
+  }
+}).listen(1111);
